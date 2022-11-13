17b300d157e354364491d32392410017277dc1e5
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-12 22:53:57 -0300

    Created city routes and linked in index

---
+++ routes/city.js
@@ -0,0 +1,6 @@
+let router = require("express").Router();
+let { create } = require("../controllers/city");
+
+router.route("/").post(create);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 let router = require('express').Router()
 
 let user = require('./user')
+let city = require('./city')
 
 router.use('/user', user)
+router.use('/city', city)
+
 module.exports = router;
