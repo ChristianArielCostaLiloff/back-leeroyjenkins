12d480ca7963c927a558a1f40eeaf05c50b4de1c
Hugo Smahlij <smahlij@gmail.com>
2022-11-14 17:16:57 -0300

    added destroy route to show

--- routes/show.js
+++ routes/show.js
@@ -1,10 +1,10 @@
 let router = require("express").Router();
 
-const { read, create, update } = require("../controllers/show");
+const { read, create, update, destroy } = require("../controllers/show");
 
 router.get("/", read)
 router.post("/", create)
 router.patch("/:id", update)
-
+router.delete("/:id", destroy)
 
 module.exports = router;
