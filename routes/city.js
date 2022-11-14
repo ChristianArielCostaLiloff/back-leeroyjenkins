c8546bc334e85640f9ee4e95d6671b6f39b255df
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-13 21:48:21 -0300

    Added delete method and route

--- routes/city.js
+++ routes/city.js
@@ -1,9 +1,10 @@
 let router = require("express").Router();
-let { create, read, readOne, update } = require("../controllers/city");
+let { create, read, readOne, update, destroy } = require("../controllers/city");
 
 router.route("/").post(create);
 router.route("/").get(read);
 router.route("/:id").get(readOne);
 router.put("/:id", update);
+router.delete("/:id", destroy)
 
 module.exports = router;
