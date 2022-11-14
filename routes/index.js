528809b84cb4fe35764c210d1da8286bb1834077
Hugo Smahlij <smahlij@gmail.com>
2022-11-13 21:27:15 -0300

    added hotel route to index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ let router = require('express').Router()
 
 let user = require('./user')
 let city = require('./city')
+let hotel = require('./hotel')
 
 router.use('/user', user)
 router.use('/city', city)
+router.use('/hotel', hotel)
 
 module.exports = router;
