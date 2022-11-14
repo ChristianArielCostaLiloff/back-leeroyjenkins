6a5cf3c88fcb1bd786bded0993739d70ad2e7b5b
Hugo Smahlij <smahlij@gmail.com>
2022-11-14 17:10:54 -0300

    added patch route to show

--- routes/show.js
+++ routes/show.js
@@ -1,8 +1,10 @@
 let router = require("express").Router();
 
-const { read, create } = require("../controllers/show");
+const { read, create, update } = require("../controllers/show");
 
 router.get("/", read)
 router.post("/", create)
+router.patch("/:id", update)
+
 
 module.exports = router;
