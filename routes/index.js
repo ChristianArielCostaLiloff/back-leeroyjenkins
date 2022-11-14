9fcb1147684f71c77cc0c348d2eacae6c9f237ee
Hugo Smahlij <smahlij@gmail.com>
2022-11-14 01:55:19 -0300

    added show route to index

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ let user = require('./user')
 let city = require('./city')
 let itinerary = require('./itinerary')
 let hotel = require('./hotel')
+let show = require('./show')
 
 router.use('/user', user)
 router.use('/city', city)
 router.use('/itinerary', itinerary)
 router.use('/hotel', hotel)
+router.use('/show', show)
 
 module.exports = router;
