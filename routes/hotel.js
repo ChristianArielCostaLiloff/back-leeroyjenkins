f2d9902a0497250c396debc0ea70961faaa87e47
Hugo Smahlij <smahlij@gmail.com>
2022-11-14 16:23:22 -0300

    created a patch route for hotel controller

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,8 +1,9 @@
 let router = require("express").Router();
-let { create, read, readOne } = require("../controllers/hotel");
+let { create, read, readOne, update } = require("../controllers/hotel");
 
 router.route("/").post(create);
 router.get("/", read);
 router.get("/:id", readOne);
+router.patch("/:id", update);
 
 module.exports = router;
