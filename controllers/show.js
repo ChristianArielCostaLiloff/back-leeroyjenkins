0a768bd68ba5bd00ce9073a058c0c0107e3adbcc
Hugo Smahlij <smahlij@gmail.com>
2022-11-14 17:03:08 -0300

    added create controller to show

--- controllers/show.js
+++ controllers/show.js
@@ -27,6 +27,21 @@ const controller = {
       });
     }
   },
+  create: async (req, res) => {
+    try {
+      let new_show = await Show.create(req.body);
+      res.status(201).json({
+        id: new_show._id,
+        success: true,
+        message: "Show created successfuly",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
 };
 
 module.exports = controller;
