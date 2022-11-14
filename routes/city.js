e9c6277129759b52cc6706d9653463ac49e6f2ff
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-13 21:25:47 -0300

    Created update method and route

--- routes/city.js
+++ routes/city.js
@@ -1,9 +1,9 @@
 let router = require("express").Router();
-let { create, read, readOne } = require("../controllers/city");
+let { create, read, readOne, update } = require("../controllers/city");
 
 router.route("/").post(create);
 router.route("/").get(read);
 router.route("/:id").get(readOne);
-
+router.put("/:id", update);
 
 module.exports = router;
