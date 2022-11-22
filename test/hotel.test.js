45e24efb10ca1e38a0e66d938ed0e71fa8013355
Hugo Smahlij <smahlij@gmail.com>
2022-11-22 03:09:12 -0300

    added hotel post test

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -15,3 +15,40 @@ describe("GET /api/hotel/", function (done) {
       });
   });
 });
+
+describe("POST /api/hotel", () => {
+  it("Capacity field is a number", (done) => {
+    request(app)
+      .post("/api/hotel")
+      .send({
+        capacity: 789722,
+      })
+      .expect((response) => {
+        assert.isNumber(response.request._data.capacity);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+  it("Hotel created successfully (201)", (done) => {
+    request(app)
+      .post("/api/hotel")
+      .send({
+        name: "Hotel Las Américas Torre del Mar",
+        photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/de/19/28/las-americas-torre-del.jpg?w=1200&h=-1&s=1",
+        capacity: 1500,
+        cityId: "63701f25d10c25267b79e291",
+        userId: "6370096b26cecde13c02e04c",
+      })
+      .expect(res => res.success === true)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
