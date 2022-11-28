faf81593a269325f6273a2aa01670712c41f5a57
Hugo Smahlij <smahlij@gmail.com>
2022-11-28 09:24:41 -0300

    created test for delete hotel

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -51,3 +51,21 @@ describe("POST /api/hotel", () => {
       });
   });
 });
+
+describe("DELETE /api/hotel", () => {
+  it("Must delete a hotel", function (done) {
+    TOKEN =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODQwNDhiMGUyNDRjYmVmMDk4ZDI5NiIsIm5hbWUiOiJHZXJhbGRvIiwicGhvdG8iOiJodHRwczovL2ltZy5ldXJvcGFwcmVzcy5lcy9mb3Rvd2ViL2ZvdG9ub3RpY2lhXzIwMjAwNzI4MTQyMjI2XzQyMC5qcGciLCJyb2xlIjoiYWRtaW4iLCJsb2dnZWQiOnRydWUsImlhdCI6MTY2OTYzNDI5MSwiZXhwIjoxNjY5NzIwNjkxfQ.90rlfFRM1Uj4cJioKujR_ZsN39CW5sg8PWaobg6o1zc";
+    request(app)
+      .delete(`/api/hotel/637cebffe52247ea19579c32`)
+     // .auth(token, { type: "bearer" })
+      .set('Authorization', `Bearer ${TOKEN}`)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
