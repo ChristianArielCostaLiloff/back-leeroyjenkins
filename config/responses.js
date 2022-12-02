9fe21b6c69760bd7ececd3eeb8f1824696b29dfb
Hugo Smahlij <smahlij@gmail.com>
2022-12-02 03:02:22 -0300

    added responses for middleware

--- config/responses.js
+++ config/responses.js
@@ -1,58 +1,74 @@
-function userExistsResponse(req,res) {
-    return res.status(400).json({
-        success: false,
-        message: 'User already exists'
-    })
+function userExistsResponse(req, res) {
+  return res.status(400).json({
+    success: false,
+    message: "User already exists",
+  });
 }
 
-function userSignedUpResponse(req,res) {
-    return res.status(201).json({
-        success: true,
-        message: 'User signed up'
-    })
+function userSignedUpResponse(req, res) {
+  return res.status(201).json({
+    success: true,
+    message: "User signed up",
+  });
 }
 
-function userSignedOutResponse(req,res) {
-    return res.status(201).json({
-        success: true,
-        message: 'User signed out'
-    })
+function userSignedOutResponse(req, res) {
+  return res.status(201).json({
+    success: true,
+    message: "User signed out",
+  });
 }
 
-function userNotFoundResponse(req,res) {
-    return res.status(404).json({
-        success: false,
-        message: 'User not found'
-    })
+function userNotFoundResponse(req, res) {
+  return res.status(404).json({
+    success: false,
+    message: "User not found",
+  });
 }
 
-function mustSignInResponse(req,res) {
-    return res.status(400).json({
-        success: false,
-        message: 'Sign in please!'
-    })
+function mustSignInResponse(req, res) {
+  return res.status(400).json({
+    success: false,
+    message: "Sign in please!",
+  });
 }
 
-function invalidCredentialsResponse(req,res) {
-    return res.status(401).json({
-        success: false,
-        message: 'Email or password incorrect'
-    })
+function invalidCredentialsResponse(req, res) {
+  return res.status(401).json({
+    success: false,
+    message: "Email or password incorrect",
+  });
 }
 
-function verifyResponse(req,res) {
-    return res.status(401).json({
-        success: false,
-        message: 'Please, verify your email account and try again'
-    })
+function verifyResponse(req, res) {
+  return res.status(401).json({
+    success: false,
+    message: "Please, verify your email account and try again",
+  });
+}
+
+function isCommentOwner(req, res) {
+  return res.status(401).json({
+    success: false,
+    message: "You must be the owner to carry out this operation",
+  });
+}
+
+function noActivity(req, res) {
+  return res.status(404).json({
+    success: false,
+    message: "Couldn't find the activity",
+  });
 }
 
 module.exports = {
-    userSignedUpResponse,
-    userExistsResponse,
-    userNotFoundResponse,
-    userSignedOutResponse,
-    mustSignInResponse,
-    invalidCredentialsResponse,
-    verifyResponse
-}
+  userSignedUpResponse,
+  userExistsResponse,
+  userNotFoundResponse,
+  userSignedOutResponse,
+  mustSignInResponse,
+  invalidCredentialsResponse,
+  verifyResponse,
+  isCommentOwner,
+  noActivity,
+};
