e6bbecb1410e31f9e8868473a929b02695417245
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-12-02 03:03:39 -0300

    Routes for reaction

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ let city = require('./city')
 let itinerary = require('./itinerary')
 let hotel = require('./hotel')
 let show = require('./show')
+let reaction = require('./reaction')
 
 router.use('/auth', user)
 router.use('/city', city)
 router.use('/itinerary', itinerary)
 router.use('/hotel', hotel)
 router.use('/show', show)
+router.use('/reaction', reaction)
 
 module.exports = router;
