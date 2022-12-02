81e8cba0f1a3b50be6145046671d4d7e38e7b2df
Hugo Smahlij <smahlij@gmail.com>
2022-12-02 03:04:33 -0300

    added schema for validation

--- schemas/comment.js
+++ schemas/comment.js
@@ -1,11 +1,10 @@
 const joi = require("joi");
 
 const schema = joi.object({
-
   showId: joi.any(),
   userId: joi.any(),
   comment: joi.string().required().min(3).messages({
-    "string.min": "Name: min 3 characters",
+    "string.min": "Comment: min 3 characters",
   }),
   userId: joi.any(),
   date: joi.date().required(),
